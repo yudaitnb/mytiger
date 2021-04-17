6759bf0f69a4b3e531a3fa605f01ca03a91f00ca
Yudai Tanabe <yudai.tnb@gmail.com>
2021-04-17 22:44:12 +0900

    reverse expected-input

--- test/util.ml
+++ test/util.ml
@@ -15,10 +15,24 @@ let lit_nil = NilExp { loc = dummy_loc; }
 
 let lit_int_minus_1 = BinOpExp {
   op = Sub;
-  e1 = IntExp { value = 0; loc=dummy_loc };
-  e2 = IntExp { value = 1; loc=dummy_loc };
+  e1 = lit_int_0;
+  e2 = lit_int_1;
   loc = dummy_loc;
 }
+
+let var_x = SimpleVar { name = "x"; loc = dummy_loc; }
+
+let varexp_x = VarExp { var = var_x; loc = dummy_loc; }
+
+let var_N = SimpleVar { name = "N"; loc = dummy_loc; }
+let varexp_N = VarExp { var = var_N; loc = dummy_loc; }
+
+let varexp_N_minus_1 =
+  BinOpExp {
+    op = Sub;
+    e1 = varexp_N;
+    e2 = lit_int_1;
+    loc = dummy_loc; }
 
 (* 
  * Lexerテストユーティリティ
@@ -44,8 +58,8 @@ let test_utility_lexer name input expected =
 let test_utility_parser name input expected =
   name >::
   (fun _ -> assert_equal ~printer:(show_exp) ~cmp:(equal_exp)
-    (parse_from_string input)
     (expected)
+    (parse_from_string input)
   )
 
 (* 
@@ -57,6 +71,6 @@ let test_utility_parser name input expected =
 let test_utility_typechecker name input expected =
   name >::
   (fun _ -> assert_equal ~printer:(T.show_ty) ~cmp:(T.equal_ty)
-    (S.typeof (parse_from_string input))
     (expected)
+    (S.typeof (parse_from_string input))
   )
